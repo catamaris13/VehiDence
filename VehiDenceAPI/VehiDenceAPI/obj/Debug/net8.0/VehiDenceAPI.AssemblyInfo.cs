b70ed974c191923b7657b7507ab54a7a58c8e97b
sasa_mezo <sasamezo77@gmail.com>
2024-05-28 14:15:42 +0300

    pentru merge

--- VehiDenceAPI/VehiDenceAPI/obj/Debug/net8.0/VehiDenceAPI.AssemblyInfo.cs
+++ VehiDenceAPI/VehiDenceAPI/obj/Debug/net8.0/VehiDenceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VehiDenceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ad5717b01a4c7b04cc9c540a34698277f6b6d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b0456726b3e14399ad79f93d6ecf88d404d4a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("VehiDenceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VehiDenceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
