af9470fdf0560c387065dab459b9fbdbd0054bc4
sasa_mezo <sasamezo77@gmail.com>
2024-05-29 14:43:47 +0300

    sters cod in plus de pe app

--- Front-End/vehidence-front/src/App.js
+++ Front-End/vehidence-front/src/App.js
@@ -1,31 +1,3 @@
-/*import Navbar from "./Navbar";
-import Home from "./Home";
-import Login from "./LoginSignUp/Login";
-import SignUp from "./LoginSignUp/SignUp"
-import { useEffect, useState } from "react";
-
-const App = () => {
-  const currentPath = window.location.pathname;
-  if (currentPath == "/login") {
-    return <Login />;
-  }
-  if(currentPath == "/signup"){
-    return <SignUp/>;
-  }
-  else {
-    return (
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Home />
-        </div>
-      </div>
-    );
-  }
-};
-
-export default App;*/
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 import Login from "./LoginSignUp/Login";
